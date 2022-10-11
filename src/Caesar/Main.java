8b65483b9248ed93aadaa7cbd50aaf8adf74bda5
Igor Lozhevskiy <igorlozhevskiy@gmail.com>
2022-10-11 13:07:17 +0300

    Add new Classes, add new jar

--- src/Caesar/Main.java
+++ src/Caesar/Main.java
@@ -2,6 +2,7 @@ package Caesar;
 
 import Caesar.Utils.Decode;
 import Caesar.Utils.Encode;
+import Caesar.Utils.FileUtils;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,29 +11,25 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Decode decoder = new Decode();
         Encode encoder = new Encode();
+        Decode decoder = new Decode();
+        FileUtils fileUtils = new FileUtils();
         String text = Files.readString(Path.of(args[1], ""));
         int key = Integer.parseInt(args[2]);
 
         if ("encode".equals(args[0])) {
-
-            String outPath1 = args[1];
-            String outPath2 = outPath1.replaceAll("Hamlet.txt", "Hamlet_encoded.txt");
-            Path outFilePath = Path.of(outPath2, "");
-
-            String encoded = encoder.encode(text, key);
-            Files.writeString(outFilePath, encoded);
+            String inputPath = args[1];
+            String outputPathString = fileUtils.renameFileEnc(inputPath,"_encoded");
+            String encodedString = encoder.encode(text, key);
+            Path newFile = Files.createFile(Path.of(outputPathString));
+            Files.writeString(newFile,encodedString);
 
         } else if ("decode".equals(args[0])) {
-
-            String outPath1 = args[1];
-            String outPath2 = outPath1.replaceAll("Hamlet.txt", "Hamlet_encoded.txt");
-            String outPath3 = outPath2.replaceAll("Hamlet_encoded.txt", "Hamlet_decoded.txt");
-            Path outFilePath = Path.of(outPath3, "");
-            String decoded = decoder.decode(text, key);
-            Files.writeString(outFilePath, decoded);
-
+            String inputPath = args[1];
+            String outputPathString = fileUtils.renameFileDec(inputPath, "_decoded");
+            String decodedString = decoder.decode(text, key);
+            Path newFile = Files.createFile(Path.of(outputPathString));
+            Files.writeString(newFile,decodedString);
         }
     }
 }
--- src/Caesar/Utils/FileUtils.java
+++ src/Caesar/Utils/FileUtils.java
@@ -1,10 +1,12 @@
 package Caesar.Utils;
 
 public class FileUtils {
-    public static final String TXT = ".txt";
+    private static final String TXT = ".txt";
 
-    public String addSuffixToFileName(String fileName, String suffix) {
+    public String renameFileEnc (String fileName, String suffix) {
         return fileName.substring(0, fileName.length() - 4) + suffix + TXT;
     }
-
+    public String renameFileDec (String fileName, String suffix) {
+        return fileName.substring(0, fileName.length() - 12) + suffix + TXT;
+    }
 }

01eb39becdf070836b37d93dc1111379909ea61f
Igor Lozhevskiy <igorlozhevskiy@gmail.com>
2022-10-12 14:24:39 +0300

    add new method of rename file in class "FileUtils"

--- src/Caesar/Main.java
+++ src/Caesar/Main.java
@@ -10,6 +10,7 @@ import java.nio.file.Path;
 
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Encode encoder = new Encode();
         Decode decoder = new Decode();
@@ -17,16 +18,16 @@ public class Main {
         String text = Files.readString(Path.of(args[1], ""));
         int key = Integer.parseInt(args[2]);
 
-        if ("encode".equals(args[0])) {
+        if ("encode".equalsIgnoreCase(args[0])) {
             String inputPath = args[1];
-            String outputPathString = fileUtils.renameFileEnc(inputPath,"_encoded");
+            String outputPathString = fileUtils.renameFile(inputPath,"_encoded");
             String encodedString = encoder.encode(text, key);
             Path newFile = Files.createFile(Path.of(outputPathString));
             Files.writeString(newFile,encodedString);
 
-        } else if ("decode".equals(args[0])) {
+        } else if ("decode".equalsIgnoreCase(args[0])) {
             String inputPath = args[1];
-            String outputPathString = fileUtils.renameFileDec(inputPath, "_decoded");
+            String outputPathString = fileUtils.renameFile(inputPath, "_decoded");
             String decodedString = decoder.decode(text, key);
             Path newFile = Files.createFile(Path.of(outputPathString));
             Files.writeString(newFile,decodedString);
--- src/Caesar/Utils/FileUtils.java
+++ src/Caesar/Utils/FileUtils.java
@@ -1,12 +1,16 @@
 package Caesar.Utils;
 
+import Caesar.Main;
+
 public class FileUtils {
     private static final String TXT = ".txt";
 
-    public String renameFileEnc (String fileName, String suffix) {
-        return fileName.substring(0, fileName.length() - 4) + suffix + TXT;
-    }
-    public String renameFileDec (String fileName, String suffix) {
-        return fileName.substring(0, fileName.length() - 12) + suffix + TXT;
+    public String renameFile (String fileName, String suffix) {
+        if (fileName.endsWith("_encoded.txt")) {
+            return fileName.substring(0, fileName.lastIndexOf("_encoded.txt")) + suffix + TXT;
+        } else if (fileName.endsWith(TXT)) {
+            return fileName.substring(0, fileName.length() - 4) + suffix + TXT;
+        }
+        return fileName;
     }
 }
